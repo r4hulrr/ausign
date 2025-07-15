df8280b6fe1e1400923429b33d1df95da9ccf9e5
Rahul Ramachandran <rahulram07r@gmail.com>
2025-07-15 17:08:36 +1000

    four flex sensors along thumb, index, middle and ring finger adc input code added

--- code/main/imu_code/imu_code.ino
+++ code/main/imu_code/imu_code.ino
@@ -1,5 +1,8 @@
 
-#define FLEX_SENSOR_PIN 4  // GPIO4 = ADC1_CH3
+#define THUMB_FLEX_SENSOR_PIN 4  // GPIO4 = ADC1_CH3
+#define INDEX_FLEX_SENSOR_PIN 6  // GPIO4 = ADC1_CH3
+#define MIDDLE_FLEX_SENSOR_PIN 15  // GPIO4 = ADC1_CH3
+#define RING_FLEX_SENSOR_PIN 17  // GPIO4 = ADC1_CH3
 
 void setup() {
   Serial.begin(115200);
@@ -8,15 +11,34 @@ void setup() {
 
 void loop() {
   const int numSamples = 50;
-  long total = 0;
+  long thumb_total = 0;
+  long index_total = 0;
+  long middle_total = 0;
+  long ring_total = 0;
 
   for (int i = 0; i < numSamples; i++) {
-    total += analogRead(FLEX_SENSOR_PIN);
+    thumb_total += analogRead(THUMB_FLEX_SENSOR_PIN);
+    index_total += analogRead(INDEX_FLEX_SENSOR_PIN);
+    middle_total += analogRead(MIDDLE_FLEX_SENSOR_PIN);
+    ring_total += analogRead(RING_FLEX_SENSOR_PIN);
     delay(2); // Short delay for better averaging
   }
 
-  int average = total / numSamples;
-  Serial.println(average);
+  int thumb_average = thumb_total / numSamples;
+  int index_average = index_total / numSamples;
+  int middle_average = middle_total / numSamples;
+  int ring_average = ring_total / numSamples;
+  Serial.print("Thumb = ");
+  Serial.print(thumb_average);
+  Serial.print(";");
+  Serial.print("Index = ");
+  Serial.print(index_average);
+  Serial.print(";");
+  Serial.print("Middle = ");
+  Serial.print(middle_average);
+  Serial.print(";");
+  Serial.print("Ring = ");
+  Serial.println(ring_average);
 
   delay(100); // Print ~10 values per second
 }
