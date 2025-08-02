dd8623b30b2ab91b0a9d7ddffb8aa79f3c6216e8
Rahul Ramachandran <rahulram07r@gmail.com>
2025-08-02 17:34:14 +1000

    added more symbols

--- python/symbols.py
+++ python/symbols.py
@@ -19,5 +19,41 @@ sign_definitions = [
         "conditions": lambda ax, ay, az, heartbeat: ax > 800,
         "sign": "3",
         "audio": "python/three.wav"
+    },
+    {
+        "fingers": {"Thumb": 1, "Index": 0, "Middle": 0, "Ring": 0, "Little": 0},
+        "conditions": lambda ax, ay, az, heartbeat: ax > 800,
+        "sign": "3",
+        "audio": "python/four.wav"
+    },
+    {
+        "fingers": {"Thumb": 0, "Index": 0, "Middle": 0, "Ring": 0, "Little": 0},
+        "conditions": lambda ax, ay, az, heartbeat: ax > 800,
+        "sign": "3",
+        "audio": "python/five.wav"
+    },
+    {
+        "fingers": {"Thumb": 0, "Index": 1, "Middle": 1, "Ring": 1, "Little": 1},
+        "conditions": lambda ax, ay, az, heartbeat: ax > 800,
+        "sign": "3",
+        "audio": "python/six.wav"
+    },
+    {
+        "fingers": {"Thumb": 0, "Index": 0, "Middle": 1, "Ring": 1, "Little": 1},
+        "conditions": lambda ax, ay, az, heartbeat: ax > 800,
+        "sign": "3",
+        "audio": "python/seven.wav"
+    },
+    {
+        "fingers": {"Thumb": 0, "Index": 0, "Middle": 0, "Ring": 1, "Little": 1},
+        "conditions": lambda ax, ay, az, heartbeat: ax > 800,
+        "sign": "3",
+        "audio": "python/eight.wav"
+    },
+    {
+        "fingers": {"Thumb": 0, "Index": 0, "Middle": 0, "Ring": 0, "Little": 1},
+        "conditions": lambda ax, ay, az, heartbeat: ax > 800,
+        "sign": "3",
+        "audio": "python/nine.wav"
     }
 ]
