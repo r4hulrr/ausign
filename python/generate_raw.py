1489c6260a72075f065f5ec1dec69a06a1de6330
Rahul Ramachandran <rahulram07r@gmail.com>
2025-08-06 16:18:16 +1000

    added exe file

--- python/generate_raw.py
+++ python/generate_raw.py
@@ -3,10 +3,10 @@ from pydub import AudioSegment
 import os
 
 # Config
-text = "nine"
+text = "love"
 volume_boost_db = 10
 output_dir = "code/data"
-raw_filename = "9.raw"
+raw_filename = "love.raw"
 raw_path = os.path.join(output_dir, raw_filename)
 
 # Generate speech
